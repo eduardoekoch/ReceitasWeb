df04654bbad4c5510ae2243b6165fea31c5307f5
eduardokoch <eduardo.ekoch@gmail.com>
2018-07-09 17:57:29 -0300

    Cadastro de cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,4 +2,19 @@ class CuisinesController < ApplicationController
   def show
       @cuisine = Cuisine.find(params[:id])
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(params.require(:cuisine).permit(:name))
+    if @cuisine.save
+      redirect_to @cuisine
+    else
+      flash[:error] = 'Você deve informar o nome da cozinha'
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   resources :recipes, only: [:show, :new, :create]
-  resources :cuisines, only: [:show]
+  resources :cuisines, only: [:show, :new, :create]
 end
